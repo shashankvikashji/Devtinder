b0f8b49fe9e420f6ae105944875c5f072365d558
shashankvikashji <shashankdubey244@gmail.com>
2025-11-25 21:20:00 +0530

    playing with routing

--- src/app.js
+++ src/app.js
@@ -3,35 +3,17 @@ const app = express();
 
 
 
-// order matter karta hai isi liye ye first pe nahi hoga 
-app.use("/user" , (req ,res)=>{
-    res.send("This is i Say order is matter !! ")
-})
-
-///->> This will only handle GET call to /user
-
-app.get( "/user" , (req,res) => {
+// req / user  , /user/xyz
+app.get( "/user/:userId/:name/:password" , (req,res) => {
+    console.log(req.params);
+    
     res.send({
         firstName: "Shashank" , lastName :"Dubey"
     })
 });
 
-/// ->> This will match all the Http method API calls to /test
-
-app.use( "/test" , (req,res) => {
-    res.send("Hello from the server ! ")
-});
 
 
-app.post("/user" , (req , res) => {
-    // Saving data to DB
-    res.send("Data is sucessfully sent to DataBase ! ")
-});
-
-app.delete("/user" , (req,res)=>{
-    res.send("Deletion is sucessfully !! ")
-})
- 
 
 app.listen(3000  , () => {
     console.log("Server is Successfully listening on port 3000 ! ");
