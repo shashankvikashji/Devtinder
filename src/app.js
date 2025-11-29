5421c8e98eb81dbb0dff876ffb3545fb6e66e9d9
shashankvikashji <shashankdubey244@gmail.com>
2025-11-29 21:29:41 +0530

    Get Api

--- src/app.js
+++ src/app.js
@@ -31,6 +31,48 @@ app.post("/signup" , async (req , res) => {
 
 
 
+// Get user by email
+
+app.get("/user" , async(req , res) => {
+
+    /// =>> This is findone method 
+     const userEmail = req.body.emailId;
+    try{
+        const user = await User.findOne({ emailId : userEmail });
+        if(!user){
+             res.status(404).send("user not found " );
+        }else{
+               res.send(user);
+        }
+          
+
+     /// =>> This is find method 
+   /*  const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({ emailId : userEmail });
+        if(users.length === 0){
+            res.status(404).send("user not found " );
+        }else{
+            res.send(users);
+        } */
+    }catch (err ) {
+        res.status(404).send("Something get wrong " );
+    }
+})
+
+
+
+// Feed api - Get /feed - get all the users from the data base 
+app.get("/feed" , async (req , res) => {
+     try{
+        const users = await User.find({});
+         res.send(users);
+     }catch(err){
+
+       res.status(404).send("Something get wrong " );
+     }
+})
+
 
 connectDB().then(()=>{
     console.log("Database connection established ......")
